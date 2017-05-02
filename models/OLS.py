a6128d238816ec096e1685ae039b387afd89e8be
robsukaldi <robsukaldi@gmail.com>
2017-05-02 21:05:57 +0200

    init aangepast

--- models/OLS.py
+++ models/OLS.py
@@ -1,6 +1,29 @@
 # Implement the OLS class here
 
 class OLS:
-    def _init__(self, x, y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    """Example function with types documented in the docstring.
+
+    `PEP 484`_ type annotations are supported. If attribute, parameter, and
+    return types are annotated according to `PEP 484`_, they do not need to be
+    included in the docstring:
+
+    Parameters
+    ----------
+    param1 : int
+        The first parameter.
+    param2 : str
+        The second parameter.
+
+    Returns
+    -------
+    bool
+        True if successful, False otherwise.
+
+    .. _PEP 484:
+        https://www.python.org/dev/peps/pep-0484/
+
+    """
