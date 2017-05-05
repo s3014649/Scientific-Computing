7b91c58089b92e4517026ee7aac25711b18e4fe2
robsukaldi <robsukaldi@gmail.com>
2017-05-05 12:18:04 +0200

    Comments verschoven

--- models/OLS.py
+++ models/OLS.py
@@ -2,16 +2,16 @@
 
 class OLS:
     def __init__(self, x, y):
-        #Method for independent and dependent variable.
+        """Method for independent and dependent variable.
+        Parameters
+        ----------
+        x : ndarray
+            The first independent parameter.
+        y : ndarray
+            The second dependent parameter.
+        """
 
         self.x = x
         self.y = y
 
 
-    """Parameters
-    ----------
-    x : ndarray
-        The first independent parameter.
-    y : ndarray
-        The second dependent parameter.
-    """
