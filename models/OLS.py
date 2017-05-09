cb994492babb97f87857c54a7e9207bc8162206b
s3014649 <dorgelo_123@hotmail.com>
2017-05-09 13:05:56 +0200

    cost function and fit
    The function is now created in the cost function and the fit returns
    only calculates the beta's now.

--- models/OLS.py
+++ models/OLS.py
@@ -1,5 +1,7 @@
-# Implement the OLS class here
+import numpy as np
+from scipy.optimize import least_squares
 
+# Implement the OLS class here
 class OLS:
     def __init__(self, x, y):
         """Method for independent and dependent variable.
@@ -13,5 +15,14 @@ class OLS:
 
         self.x = x
         self.y = y
+
+    def _cost_function(self):
+        def fun(j, x, y):
+            return ((j * x) - y)*((j * x) - y)
+
+    def fit(self):
+        x0 = np.repeat(1, len(self.x))
+        res = least_squares(self._cost_function(), x0, args=(self.x, self.y))
+        self.beta = res.x
 
 
